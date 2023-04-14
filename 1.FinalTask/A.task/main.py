e2231a1b932a57148829a9c70364d275b0e89f9c
Alexander <piachev.a@ya.ru>
2023-04-14 23:37:21 +1000

    Fix bugs

--- 1.FinalTask/A.task/main.py
+++ 1.FinalTask/A.task/main.py
@@ -1,19 +1,10 @@
-class Queue:
+class DecQueue:
     def __init__(self, n):
         self.queue = [None] * n
         self.max_n = n
         self.head = 0
         self.tail = 0
         self.size = 0
-        self.COMMAND = {
-            'push_front': self.push_front,
-            'push_back': self.push_back,
-            'pop_front': self.pop_front,
-            'pop_back': self.pop_back,
-            'pop': self.pop_back,
-            'peek': self.peek,
-            'size': self.size_print,
-        }
 
     def is_empty(self):
         return self.size == 0
@@ -24,7 +15,7 @@ class Queue:
             self.tail = (self.tail + 1) % self.max_n
             self.size += 1
         else:
-            print('error')
+            return "error"
 
     def push_back(self, x):
         if self.size != self.max_n:
@@ -32,56 +23,55 @@ class Queue:
             self.queue[self.head] = x
             self.size += 1
         else:
-            print('error')
+            return "error"
 
     def pop_back(self):
         if self.is_empty():
-            return print('error')
+            return "error"
         x = self.queue[self.head]
         self.queue[self.head] = None
         self.head = (self.head + 1) % self.max_n
         self.size -= 1
-        return print(x)
+        return x
 
     def pop_front(self):
         if self.is_empty():
-            return print('error')
+            return "error"
         self.tail = (self.tail - 1) % self.max_n
         x = self.queue[self.tail]
         self.queue[self.tail] = None
         self.size -= 1
-        return print(x)
-
-    def peek(self):
-        if self.is_empty():
-            return print('None')
-        return print(self.queue[self.head])
-
-    def size_print(self):
-        return print(self.size)
+        return x
 
 
 def load_data():
-    file = open('./input.txt', 'rt')
-    commands_len = int(file.readline().strip())
-    max_len_queue = int(file.readline().strip())
-    commands = [x for x in file.read().split('\n')]
-    queue = Queue(max_len_queue)
-    for command in commands:
-        stack_interface(command, queue)
+    file = open("./input.txt", "rt")
+    _ = int(file.readline())
+    max_len_queue = int(file.readline())
+    commands = [x for x in file.read().split("\n")]
+    queue = DecQueue(max_len_queue)
+    for cmd in commands:
+        queue_interface(cmd, queue)
 
 
-def stack_interface(data: str, queue: Queue):
-    command = data.split()
-    length = len(command)
+def queue_interface(data: str, queue: DecQueue):
+    command = {
+        "push_front": queue.push_front,
+        "push_back": queue.push_back,
+        "pop_front": queue.pop_front,
+        "pop_back": queue.pop_back,
+    }
+    cmd = data.split()
+    length = len(cmd)
+    msg = None
     if length > 1:
-        queue.COMMAND[data.split()[0]](int(data.split()[1]))
+        msg = command[cmd[0]](int(cmd[1]))
     elif length == 1:
-        queue.COMMAND[data.split()[0]]()
+        msg = command[cmd[0]]()
+
+    if msg is not None:
+        print(msg)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_data()
-    # import timeit
-    #
-    # print(timeit.timeit('load_data()', number=1000, setup='from __main__ import load_data'))
